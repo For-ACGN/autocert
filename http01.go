87bee16912b6508448f57ba7a03fe9e10a8c7a29
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-15 23:11:32 +0800

    finish a http server for process CA challenge with HTTP-01.

---
+++ http01.go
@@ -0,0 +1,31 @@
+package autocert
+
+import (
+	"net"
+	"net/http"
+)
+
+type http01 struct {
+	server *http.Server
+}
+
+func newHTTP01(handler http.Handler) *http01 {
+	return &http01{
+		server: &http.Server{Handler: handler},
+	}
+}
+
+func (h *http01) Start() error {
+	listener, err := net.Listen("tcp", ":80")
+	if err != nil {
+		return err
+	}
+	go func() {
+		_ = h.server.Serve(listener)
+	}()
+	return nil
+}
+
+func (h *http01) Close() error {
+	return h.server.Close()
+}
