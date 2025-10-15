7a5324d21cfcb4a3bcbbd78398c9c62b7d1a0e24
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-15 22:46:46 +0800

    finish a portmap for forward 443 port to listener's port.

---
+++ portmap.go
@@ -0,0 +1,63 @@
+package autocert
+
+import (
+	"io"
+	"net"
+)
+
+type portmap struct {
+	network  string
+	port     string
+	listener net.Listener
+}
+
+func newPortmap(network, port string) *portmap {
+	return &portmap{
+		network: network,
+		port:    port,
+	}
+}
+
+func (p *portmap) Start() error {
+	listener, err := net.Listen("tcp", ":443")
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+			go p.forward(conn)
+		}
+	}()
+	return nil
+}
+
+func (p *portmap) forward(conn net.Conn) {
+	var host string
+	switch p.network {
+	case "tcp", "tcp4":
+		host = "127.0.0.1"
+	case "tcp6":
+		host = "::1"
+	}
+	address := net.JoinHostPort(host, p.port)
+	remote, err := net.Dial(p.network, address)
+	if err != nil {
+		return
+	}
+	go func() {
+		defer func() { _ = remote.Close() }()
+		_, _ = io.Copy(conn, remote)
+	}()
+	go func() {
+		defer func() { _ = conn.Close() }()
+		_, _ = io.Copy(remote, conn)
+	}()
+}
+
+func (p *portmap) Stop() error {
+	return p.listener.Close()
+}
