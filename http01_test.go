1f67268b53893198c769c3e267562345152e8c03
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-18 21:30:36 +0800

    add TestHTTP01 for check the basic methods about start and stop.

--- http01_test.go
+++ http01_test.go
@@ -1,1 +1,23 @@
 package autocert
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTTP01(t *testing.T) {
+	mux := http.NewServeMux()
+	http01 := newHTTP01(mux)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := http01.Start(ctx)
+	require.NoError(t, err)
+
+	err = http01.Stop(ctx)
+	require.NoError(t, err)
+}
