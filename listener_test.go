4a4b509c0b5f4e2a4b890009f8f334e42169c8ae
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-18 21:53:23 +0800

    add TestListen for check method acme-tls and http01, use Domains.

--- listener_test.go
+++ listener_test.go
@@ -1,11 +1,42 @@
 package autocert
 
 import (
+	"net"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestListen(t *testing.T) {
-	require.NoError(t, nil)
+	defer func() {
+		err := os.RemoveAll("certs")
+		require.NoError(t, err)
+	}()
+
+	config := &Config{
+		Domains: []string{"example.com"},
+	}
+
+	t.Run("acme-tls", func(t *testing.T) {
+		listener, err := Listen("tcp", "127.0.0.1:4000", config)
+		require.NoError(t, err)
+
+		err = listener.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("http01", func(t *testing.T) {
+		used, err := net.Listen("tcp", ":443")
+		require.NoError(t, err)
+
+		listener, err := Listen("tcp", "127.0.0.1:4000", config)
+		require.NoError(t, err)
+
+		err = listener.Close()
+		require.NoError(t, err)
+
+		err = used.Close()
+		require.NoError(t, err)
+	})
 }
