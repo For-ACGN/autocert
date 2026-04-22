88d6550d149a30a8f839b13436d382e06c68ee78
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-22 13:03:22 +0800

    replace TestPortmap to TestTLS01, rename variables about portmap.

--- tls01_test.go
+++ tls01_test.go
@@ -9,19 +9,19 @@ import (
 	"github.com/For-ACGN/autocert/certmgr"
 )
 
-func TestPortmap(t *testing.T) {
+func TestTLS01(t *testing.T) {
 	manager := &certmgr.Manager{
 		Prompt:     certmgr.AcceptTOS,
 		HostPolicy: certmgr.HostWhitelist("example.com"),
 	}
-	portmap := newPortmap(manager.GetCertificate)
+	tls01 := newTLS01(manager.GetCertificate)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := portmap.Start(ctx)
+	err := tls01.Start(ctx)
 	require.NoError(t, err)
 
-	err = portmap.Stop()
+	err = tls01.Stop()
 	require.NoError(t, err)
 }
